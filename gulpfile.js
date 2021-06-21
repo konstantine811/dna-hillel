4514877699c3943e602cf0863892a72051d7486c
Abramov Constantine <k.abramkin@kernel.ua>
2021-06-21 16:21:00 +0300

    add first gulp boilerplate

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+const { src, dest, watch, series } = require('gulp');
+
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+
+const browserSync = require('browser-sync').create();
+
+// Compile pug files into HTML
+function pugHtml() {
+  return src('src/pug/pages/*.pug')
+    .pipe(
+      pug({
+        pretty: true,
+      })
+    )
+    .pipe(dest('dist'));
+}
+
+function html() {
+  return src('src/**/*.html').pipe(dest('dist'));
+}
+
+// scripts
+function scripts() {
+  return src('src/js/**/*.js')
+    .pipe(
+      babel({
+        presets: ['@babel/env'],
+      })
+    )
+    .pipe(dest('dist'));
+}
+
+// Compile sass files into CSS
+function styles() {
+  return src('src/styles/main.scss')
+    .pipe(
+      sass({
+        includePaths: ['src/sass'],
+        errLogToConsole: true,
+        outputStyle: 'expanded',
+        onError: browserSync.notify,
+      })
+    )
+    .pipe(dest('dist/css'))
+    .pipe(browserSync.stream());
+}
+
+// Copy assets
+function assets() {
+  return src('src/assets/**/*').pipe(dest('dist/assets/'));
+}
+
+// Serve and watch sass/pug files for changes
+function watchAndServe() {
+  browserSync.init({
+    server: 'dist',
+  });
+
+  watch('src/styles/**/*.scss', styles);
+  watch('src/pug/pages/*.pug', pugHtml);
+  // watch('src/**/*.html', html);
+  watch('src/assets/**/*', assets);
+  watch('src/js/**.*', scripts);
+  watch('dist/*.html').on('change', browserSync.reload);
+}
+
+exports.html = html;
+exports.pugHtml = pugHtml;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watch = watchAndServe;
+exports.default = series(pugHtml, scripts, styles, assets, watchAndServe);
---
+++ src/README.md
@@ -0,0 +1,6 @@
+gulp_boilerplate
+Gulp Boilerplate with SCSS, PUG, JS compillers and watchers
+
+1. После того как склонировали проект откройте консоль в текущей папке проекта и введите комманду: npm i - для установки всех зависимостей из package.json
+
+2. Запустите команду в консоле gulp watch
